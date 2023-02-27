707c1a5b3a7ca1ff3b8d60eea4c4aabf556a4be5
unknown <pedrojok@pm.me>
2023-02-27 15:07:39 +0700

    Update: add all missing favicons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,12 @@ const MainPage: NextPage = () => {
             <Head>
                 <title>Move My Wallet</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+                <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico" />
+                <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+                <link rel="icon" type="image/png" sizes="32x32" href="%PUBLIC_URL%/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="%PUBLIC_URL%/favicon-16x16.png" />
+                <link rel="apple-touch-icon" href="%PUBLIC_URL%/apple-touch-icon.png" />
             </Head>
 
             <HeaderPage />
