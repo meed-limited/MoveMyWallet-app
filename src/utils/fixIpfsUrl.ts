7840f1b4b6e3723432e0b9d3add7e12b8db46c48
unknown <pedrojok@pm.me>
2023-02-28 07:30:04 +0700

    Fix: add an IPFS check in fixIpfsUrl function

--- src/utils/fixIpfsUrl.ts
+++ src/utils/fixIpfsUrl.ts
@@ -1,6 +1,10 @@
 export const fixIpfsUrl = (url: string): string => {
-    const startIndex = url.indexOf("https://") + "https://".length;
-    const endIndex = url.indexOf("/ipfs/");
-    const ipfsHash = url.substring(startIndex, endIndex);
-    return url.replace(`https://${ipfsHash}/ipfs/`, "https://ipfs.moralis.io:2053/ipfs/");
+    const hasHttpAndIpfs = url.includes("https://") && url.includes("/ipfs/");
+    if (hasHttpAndIpfs) {
+        const startIndex = url.indexOf("https://") + "https://".length;
+        const endIndex = url.indexOf("/ipfs/");
+        const ipfsHash = url.substring(startIndex, endIndex);
+        return url.replace(`https://${ipfsHash}/ipfs/`, "https://ipfs.moralis.io:2053/ipfs/");
+    }
+    return url;
 };
